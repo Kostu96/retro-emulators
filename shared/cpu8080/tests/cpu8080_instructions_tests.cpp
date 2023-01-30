4341ac11a91123a23b5f3a8a283f27eec13b4b8a
Konstanty <Kostu96@gmail.com>
2023-01-30 22:53:58 +0100

    WIP

--- shared/cpu8080/tests/cpu8080_instructions_tests.cpp
+++ shared/cpu8080/tests/cpu8080_instructions_tests.cpp
@@ -5,23 +5,59 @@
 struct CPU8080InstructionsTests :
 	public testing::Test
 {
-	CPU8080 cpu;
+	struct CPUState
+	{
+		u16 PC;
+		u16 SP;
+		u16 AF;
+		u16 BC;
+		u16 DE;
+		u16 HL;
+	};
+
+	struct {
+		u8 buffer[2];
+
+		u8 read(u16 address) const { return buffer[address]; }
+	} ROM;
+
+	CPU8080 CPU;
+	CPUState referenceCPUState;
 
 	CPU8080InstructionsTests() :
-		cpu{ CPU8080::Mode::Intel8080 } {}
+		CPU{ CPU8080::Mode::Intel8080 }
+	{
+		CPU.map(ROM, { 0x0000, 0x0001 });
+	}
 
 	void SetUp() override
 	{
-		cpu.reset();
+		CPU.reset();
+
+		referenceCPUState.PC = CPU.getPC();
+		referenceCPUState.SP = CPU.getSP();
+		referenceCPUState.AF = CPU.getAF();
+		referenceCPUState.BC = CPU.getBC();
+		referenceCPUState.DE = CPU.getDE();
+		referenceCPUState.HL = CPU.getHL();
 	}
 
 	void TearDown() override
 	{
-
+		EXPECT_EQ(referenceCPUState.PC, CPU.getPC());
+		EXPECT_EQ(referenceCPUState.SP, CPU.getSP());
+		EXPECT_EQ(referenceCPUState.AF, CPU.getAF());
+		EXPECT_EQ(referenceCPUState.BC, CPU.getBC());
+		EXPECT_EQ(referenceCPUState.DE, CPU.getDE());
+		EXPECT_EQ(referenceCPUState.HL, CPU.getHL());
 	}
 };
 
 TEST_F(CPU8080InstructionsTests, NOPTest)
 {
+	ROM.buffer[0] = 0x00;
 
+	CPU.clock();
+
+	referenceCPUState.PC = 0x0001;
 }
