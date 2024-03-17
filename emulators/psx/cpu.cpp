51b6c3a880081f9d94965a97056b23166001be9c
Konstanty <kostu96@gmail.com>
2024-03-17 23:51:30 +0100

    changes

--- emulators/psx/cpu.cpp
+++ emulators/psx/cpu.cpp
@@ -4,86 +4,106 @@
 
 namespace PSX {
 
-	void CPU::reset()
-	{
-		m_PC = 0xBFC00000;
-	}
+    void CPU::reset()
+    {
+        m_SR = 0;
 
-	void CPU::clock()
-	{
-		static Instruction next_instruction = 0;
-		Instruction inst = next_instruction;
-		next_instruction = load32(m_PC);
-		m_PC += 4;
+        m_PC = 0xBFC00000;
+    }
 
-		switch (inst.opcode())
-		{
-		case 0x00:
-			switch (inst.subfn())
-			{
-			case 0x00: op_SLL(inst.regD(), inst.regT(), inst.shift()); break;
-			default:
-				assert(false && "Unhandled opcode!");
-			}
-			break;
+    void CPU::clock()
+    {
+        static Instruction next_instruction = 0;
+        Instruction inst = next_instruction;
+        next_instruction = load32(m_PC);
+        m_PC += 4;
 
-		case 0x02: op_J(inst.imm_jump()); break;
+        switch (inst.opcode())
+        {
+        case 0x00:
+            switch (inst.subfn())
+            {
+            case 0x00: op_SLL(inst.regD(), inst.regT(), inst.shift()); break;
 
-		case 0x09: op_ADDIU(inst.regT(), inst.regS(), inst.imm_se()); break;
+            case 0x25: op_OR(inst.regD(), inst.regS(), m_regs[inst.regT()]); break;
+            default:
+                assert(false && "Unhandled opcode!");
+            }
+            break;
 
-		case 0x0D: op_ORI(inst.regT(), inst.regS(), inst.imm()); break;
+        case 0x02: op_J(inst.imm_jump()); break;
 
-		case 0x0F: op_LUI(inst.regT(), inst.imm()); break;
+        case 0x09: op_ADDIU(inst.regT(), inst.regS(), inst.imm_se()); break;
 
-		case 0x2B: op_SW(inst.regT(), inst.regS(), inst.imm_se()); break;
-		default:
-			assert(false && "Unhandled opcode!");
-		}
+        case 0x0D: op_OR(inst.regT(), inst.regS(), inst.imm()); break;
 
-		assert(m_regs[0] == 0 && "GPR zero value was changed!");
-	}
+        case 0x0F: op_LUI(inst.regT(), inst.imm()); break;
+        case 0x10:
+            switch (inst.copfn())
+            {
+            case 0x04: op_MTC0(inst.regD(), inst.regT()); break;
+            default:
+                assert(false && "Unhandled opcode!");
+            }
+            break;
 
-	CPU::CPU()
-	{
-		m_regs[0] = 0;
-	}
+        case 0x2B: op_SW(inst.regT(), inst.regS(), inst.imm_se()); break;
+        default:
+            assert(false && "Unhandled opcode!");
+        }
 
-	void CPU::setReg(u32 index, u32 value)
-	{
-		assert(index < 32 && "Index out of bounds!");
+        assert(m_regs[0] == 0 && "GPR zero value was changed!");
+    }
 
-		m_regs[index] = value;
-		m_regs[0] = 0;
-	}
+    CPU::CPU()
+    {
+        m_regs[0] = 0;
+    }
 
-	void CPU::op_SLL(u32 dIndex, u32 tIndex, u32 shift)
-	{
-		setReg(dIndex, m_regs[tIndex] << shift);
-	}
+    void CPU::setReg(u32 index, u32 value)
+    {
+        assert(index < 32 && "Index out of bounds!");
 
-	void CPU::op_J(u32 immediate)
-	{
-		m_PC = (m_PC & 0xF0000000) | immediate;
-	}
+        m_regs[index] = value;
+        m_regs[0] = 0;
+    }
 
-	void CPU::op_ADDIU(u32 tIndex, u32 sIndex, u32 immediate)
-	{
-		setReg(tIndex, m_regs[sIndex] + immediate);
-	}
+    void CPU::op_MTC0(u32 copIndex, u32 cpuIndex)
+    {
+        m_copRegs[copIndex] = m_regs[cpuIndex];
+    }
 
-	void CPU::op_ORI(u32 tIndex, u32 sIndex, u32 immediate)
-	{
-		setReg(tIndex, m_regs[sIndex] | immediate);
-	}
+    void CPU::op_SLL(u32 dIndex, u32 tIndex, u32 shift)
+    {
+        setReg(dIndex, m_regs[tIndex] << shift);
+    }
 
-	void CPU::op_LUI(u32 index, u32 immediate)
-	{
-		setReg(index, immediate << 16);
-	}
+    void CPU::op_OR(u32 targetIndex, u32 lhsIndex, u32 rhsValue)
+    {
+        setReg(targetIndex, m_regs[lhsIndex] | rhsValue);
+    }
 
-	void CPU::op_SW(u32 tIndex, u32 sIndex, u32 immediate)
-	{
-		store32(m_regs[sIndex] + immediate, m_regs[tIndex]);
-	}
+    void CPU::op_J(u32 immediate)
+    {
+        m_PC = (m_PC & 0xF0000000) | immediate;
+    }
+
+    void CPU::op_ADDIU(u32 tIndex, u32 sIndex, u32 immediate)
+    {
+        setReg(tIndex, m_regs[sIndex] + immediate);
+    }
+
+    void CPU::op_LUI(u32 index, u32 immediate)
+    {
+        setReg(index, immediate << 16);
+    }
+
+    void CPU::op_SW(u32 tIndex, u32 sIndex, u32 immediate)
+    {
+        if (m_SR & 0x10000)
+            return; // isolated cache bit is set
+
+        store32(m_regs[sIndex] + immediate, m_regs[tIndex]);
+    }
 
 } // namespace PSX
--- emulators/psx/cpu.hpp
+++ emulators/psx/cpu.hpp
@@ -5,63 +5,72 @@
 
 namespace PSX {
 
-	class CPU
-	{
-	public:
-		using Read8MemoryCallback = std::function<u8(u32)>;
-		using Read32MemoryCallback = std::function<u32(u32)>;
-		using Write8MemoryCallback = std::function<void(u32, u8)>;
-		using Write32MemoryCallback = std::function<void(u32, u32)>;
-		void mapRead8MemoryCallback(Read8MemoryCallback callback) { load8 = callback; }
-		void mapRead32MemoryCallback(Read32MemoryCallback callback) { load32 = callback; }
-		void mapWrite8MemoryCallback(Write8MemoryCallback callback) { store8 = callback; }
-		void mapWrite32MemoryCallback(Write32MemoryCallback callback) { store32 = callback; }
+    class CPU
+    {
+    public:
+        using Read8MemoryCallback = std::function<u8(u32)>;
+        using Read32MemoryCallback = std::function<u32(u32)>;
+        using Write8MemoryCallback = std::function<void(u32, u8)>;
+        using Write32MemoryCallback = std::function<void(u32, u32)>;
+        void mapRead8MemoryCallback(Read8MemoryCallback callback) { load8 = callback; }
+        void mapRead32MemoryCallback(Read32MemoryCallback callback) { load32 = callback; }
+        void mapWrite8MemoryCallback(Write8MemoryCallback callback) { store8 = callback; }
+        void mapWrite32MemoryCallback(Write32MemoryCallback callback) { store32 = callback; }
 
-		void reset();
-		void clock();
+        void reset();
+        void clock();
 
-		CPU();
-		CPU(CPU&) = delete;
-		CPU& operator=(CPU&) = delete;
-	private:
-		struct Instruction
-		{
-			u32 word;
+        CPU();
+        CPU(CPU&) = delete;
+        CPU& operator=(CPU&) = delete;
+    private:
+        struct Instruction
+        {
+            u32 word;
 
-			Instruction(u32 _word) : word(_word) {}
+            Instruction(u32 _word) : word(_word) {}
 
-			u32 opcode() const { return word >> 26; }
-			u32 subfn() const { return word & 0x3F; }
-			u32 copfn() const { return (word >> 21) & 0x1F; }
-			u32 regS() const { return (word >> 21) & 0x1F; }
-			u32 regT() const { return (word >> 16) & 0x1F; }
-			u32 regD() const { return (word >> 11) & 0x1F; }
-			u32 imm() const { return word & 0xFFFF; }
-			u32 imm_se() const { return static_cast<s16>(word & 0xFFFF); }
-			u32 imm_jump() const { return (word & 0x3FFFFFF) << 2; }
-			u32 shift() const { return (word >> 6) & 0x1F; }
-		};
+            u32 opcode() const { return word >> 26; }
+            u32 subfn() const { return word & 0x3F; }
+            u32 copfn() const { return (word >> 21) & 0x1F; }
+            u32 regS() const { return (word >> 21) & 0x1F; }
+            u32 regT() const { return (word >> 16) & 0x1F; }
+            u32 regD() const { return (word >> 11) & 0x1F; }
+            u32 imm() const { return word & 0xFFFF; }
+            u32 imm_se() const { return static_cast<s16>(word & 0xFFFF); }
+            u32 imm_jump() const { return (word & 0x3FFFFFF) << 2; }
+            u32 shift() const { return (word >> 6) & 0x1F; }
+        };
 
-		Read8MemoryCallback load8 = nullptr;
-		Read32MemoryCallback load32 = nullptr;
-		Write8MemoryCallback store8 = nullptr;
-		Write32MemoryCallback store32 = nullptr;
-		void setReg(u32 index, u32 value);
+        Read8MemoryCallback load8 = nullptr;
+        Read32MemoryCallback load32 = nullptr;
+        Write8MemoryCallback store8 = nullptr;
+        Write32MemoryCallback store32 = nullptr;
+        void setReg(u32 index, u32 value);
 
-		void op_SLL(u32 dIndex, u32 tIndex, u32 shift);
+        void op_MTC0(u32 copIndex, u32 cpuIndex);
 
-		void op_J(u32 immediate);
+        void op_SLL(u32 dIndex, u32 tIndex, u32 shift);
+        void op_OR(u32 targetIndex, u32 lhsIndex, u32 rhsValue);
+        void op_J(u32 immediate);
+        void op_ADDIU(u32 tIndex, u32 sIndex, u32 immediate);
+        void op_LUI(u32 index, u32 immediate);
+        void op_SW(u32 tIndex, u32 sIndex, u32 immediate);
 
-		void op_ADDIU(u32 tIndex, u32 sIndex, u32 immediate);
+        // COP0:
+        union {
+            struct {
+                u32 m_regs1[12]; // 0-11
+                u32 m_SR; // 12
+                u32 m_regs2[19]; // 13-31
 
-		void op_ORI(u32 tIndex, u32 sIndex, u32 immediate);
-
-		void op_LUI(u32 index, u32 immediate);
-
-		void op_SW(u32 tIndex, u32 sIndex, u32 immediate);
-
-		u32 m_PC;
-		u32 m_regs[32];
-	};
+            };
+            u32 m_copRegs[32];
+        };
+        
+        // CPU:
+        u32 m_PC;
+        u32 m_regs[32];
+    };
 
 } // namespace PSX
