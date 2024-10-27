0192da131c1f26cb2114fd168a3c0d76bfc29ab0
Kostu96 <kostu96@gmail.com>
2024-10-27 19:36:38 +0100

    Add more 8080 tests

--- shared/tests/devices/cpu8080/cpu8080_transfer_tests.cpp
+++ shared/tests/devices/cpu8080/cpu8080_transfer_tests.cpp
@@ -1007,3 +1007,152 @@ TEST_F(CPU8080TransferTests, MOV_A_ATest)
 	preExecutionState.PC = 0x0001;
 	compareCPUStates(preExecutionState, postExecutionState);
 }
+
+TEST_F(CPU8080TransferTests, MVITest)
+{
+	rom[0x0] = 0x06;
+	rom[0x1] = 0x01; // MVI B, 0x01
+
+	rom[0x2] = 0x0E;
+	rom[0x3] = 0x23; // MVI C, 0x23
+
+	rom[0x4] = 0x16;
+	rom[0x5] = 0x45; // MVI D, 0x45
+
+	rom[0x6] = 0x1E;
+	rom[0x7] = 0x67; // MVI E, 0x67
+
+	rom[0x8] = 0x26;
+	rom[0x9] = 0x80; // MVI H, 0x80
+
+	rom[0xA] = 0x2E;
+	rom[0xB] = 0x01; // MVI L, 0x01
+
+	rom[0xC] = 0x36;
+	rom[0xD] = 0xCD; // MVI [HL], 0xCD
+
+	rom[0xE] = 0x3E;
+	rom[0xF] = 0xEF; // MVI A, 0xEF
+
+	auto preExecutionState = captureCPUState();
+	u8 clocks = 7 * 7 + 10;
+	while (clocks--)
+		cpu.clock();
+	const auto postExecutionState = captureCPUState();
+
+	preExecutionState.BC = 0x0123;
+	preExecutionState.DE = 0x4567;
+	preExecutionState.HL = 0x8001;
+	preExecutionState.A = 0xEF;
+	preExecutionState.PC = 0x0010;
+	compareCPUStates(preExecutionState, postExecutionState);
+
+	EXPECT_EQ(ram[1], 0xCD);
+}
+
+TEST_F(CPU8080TransferTests, LXITest)
+{
+	rom[0x0] = 0x01;
+	rom[0x1] = 0x23;
+	rom[0x2] = 0x01; // LXI BC, 0x0123
+
+	rom[0x3] = 0x11;
+	rom[0x4] = 0x67;
+	rom[0x5] = 0x45; // LXI DE, 0x4567
+
+	rom[0x6] = 0x21;
+	rom[0x7] = 0xAB;
+	rom[0x8] = 0x89; // LXI HL, 0x89AB
+
+	rom[0x9] = 0x31;
+	rom[0xA] = 0xEF;
+	rom[0xB] = 0xCD; // LXI SP, 0xCDEF
+
+	auto preExecutionState = captureCPUState();
+	u8 clocks = 4 * 10;
+	while (clocks--)
+		cpu.clock();
+	const auto postExecutionState = captureCPUState();
+
+	preExecutionState.BC = 0x0123;
+	preExecutionState.DE = 0x4567;
+	preExecutionState.HL = 0x89AB;
+	preExecutionState.SP = 0xCDEF;
+	preExecutionState.PC = 0x000C;
+	compareCPUStates(preExecutionState, postExecutionState);
+}
+
+TEST_F(CPU8080TransferTests, LDATest)
+{
+	rom[0x0] = 0x3A;
+	rom[0x1] = 0x00;
+	rom[0x2] = 0x80; // LDA 0x8000
+	ram[0x0] = 0x42;
+
+	auto preExecutionState = captureCPUState();
+	u8 cycles = 13;
+	while (cycles--)
+		cpu.clock();
+	auto postExecutionState = captureCPUState();
+
+	preExecutionState.PC = 0x0003;
+	preExecutionState.A = 0x42;
+	compareCPUStates(preExecutionState, postExecutionState);
+}
+
+TEST_F(CPU8080TransferTests, STATest)
+{
+	rom[0x0] = 0x32;
+	rom[0x1] = 0x00;
+	rom[0x2] = 0x80; // STA 0x8000
+	cpu.getState().A = 0x42;
+
+	auto preExecutionState = captureCPUState();
+	u8 cycles = 13;
+	while (cycles--)
+		cpu.clock();
+	auto postExecutionState = captureCPUState();
+
+	preExecutionState.PC = 0x0003;
+	compareCPUStates(preExecutionState, postExecutionState);
+	EXPECT_EQ(ram[0], 0x42);
+}
+
+TEST_F(CPU8080TransferTests, LHLDTest)
+{
+	rom[0x0] = 0x2A;
+	rom[0x1] = 0x00;
+	rom[0x2] = 0x80; // LHLD 0x8000
+	ram[0x0] = 0xAD;
+	ram[0x1] = 0xDE;
+
+	auto preExecutionState = captureCPUState();
+	u8 cycles = 16;
+	while (cycles--)
+		cpu.clock();
+	auto postExecutionState = captureCPUState();
+
+	preExecutionState.PC = 0x0003;
+	preExecutionState.HL = 0xDEAD;
+	compareCPUStates(preExecutionState, postExecutionState);
+}
+
+TEST_F(CPU8080TransferTests, SHLDTest)
+{
+	rom[0x0] = 0x22;
+	rom[0x1] = 0x00;
+	rom[0x2] = 0x80; // SHLD 0x8000
+	cpu.getState().HL = 0xDEAD;
+
+	auto preExecutionState = captureCPUState();
+	u8 cycles = 16;
+	while (cycles--)
+		cpu.clock();
+	auto postExecutionState = captureCPUState();
+
+	preExecutionState.PC = 0x0003;
+	preExecutionState.HL = 0xDEAD;
+	compareCPUStates(preExecutionState, postExecutionState);
+	EXPECT_EQ(ram[0], 0xAD);
+	EXPECT_EQ(ram[1], 0xDE);
+}
