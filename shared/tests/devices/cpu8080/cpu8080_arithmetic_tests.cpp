57ddc9280e0365b5865ff752d8c16f7e8fffe8f4
Kostu96 <kostu96@gmail.com>
2024-10-28 16:32:03 +0100

    wip on tests

--- shared/tests/devices/cpu8080/cpu8080_arithmetic_tests.cpp
+++ shared/tests/devices/cpu8080/cpu8080_arithmetic_tests.cpp
@@ -2,3 +2,37 @@
 
 using CPU8080ArithmeticTests = CPU8080Tests;
 
+TEST_F(CPU8080ArithmeticTests, ADDBStandardTest)
+{
+	rom[0x0] = 0x80;
+	cpu.getState().A = 0x3;
+	cpu.getState().B = 0x5;
+
+	auto preExecutionState = captureCPUState();
+	u8 cycles = 4;
+	while (cycles--)
+		cpu.clock();
+	auto postExecutionState = captureCPUState();
+
+	preExecutionState.PC = 0x0001;
+	preExecutionState.A = 0x8;
+	compareCPUStates(preExecutionState, postExecutionState);
+}
+
+TEST_F(CPU8080ArithmeticTests, ADDBWithCarryTest)
+{
+	rom[0x0] = 0x80;
+	cpu.getState().A = 0xF7;
+	cpu.getState().B = 0x82;
+
+	auto preExecutionState = captureCPUState();
+	u8 cycles = 4;
+	while (cycles--)
+		cpu.clock();
+	auto postExecutionState = captureCPUState();
+
+	preExecutionState.PC = 0x0001;
+	preExecutionState.A = 0x79;
+	preExecutionState.F.Carry = 1;
+	compareCPUStates(preExecutionState, postExecutionState);
+}
