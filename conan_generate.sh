69930277e9337c3754067cff7effe3495687f788
Kostu96 <kostu96@gmail.com>
2025-10-24 22:41:24 +0200

    WIP
    Signed-off-by: Kostu96 <kostu96@gmail.com>

--- conan_generate.sh
+++ conan_generate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-conan install . -s build_type=Debug --build=missing
-conan install . -s build_type=Release --build=missing
+conan install . --settings=build_type=Debug --build=missing
+conan install . --settings=build_type=Release --build=missing
 
 cmake --preset conan-debug
 cmake --preset conan-release
