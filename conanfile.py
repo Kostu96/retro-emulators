9872939667a98748b01700fe84780def9551b53e
Kostu96 <kostu96@gmail.com>
2025-10-29 08:43:19 +0100

    WIP
    Signed-off-by: Kostu96 <kostu96@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -7,14 +7,14 @@ class RetroEmulatorsConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
     
-    
     def requirements(self):
         self.requires("gtest/[>1.17.0]")
         self.requires("portable-file-dialogs/0.1.0")
-    
+        self.requires("glfw/3.4")
+
     
     def layout(self):
-        cmake_layout(self)
+        cmake_layout(self, generator="Ninja Multi-Config")
     
     
     def build(self):
